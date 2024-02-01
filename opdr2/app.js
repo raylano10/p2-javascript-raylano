230b5df7e8188615e68ad86d1a5858881857b38f
raylano10 <143798089+raylano10@users.noreply.github.com>
2024-02-01 14:08:57 +0100

    hg

---
+++ opdr2/app.js
@@ -0,0 +1,11 @@
+let output = document.getElementById ("weergave")
+
+for(let i = 1; i < 101; i++){
+    output.innerHTML += i + "<br>";
+}
+
+
+
+for(let o = 100; o >=0; o--){
+    output.innerHTML += o + "<br>"
+}
