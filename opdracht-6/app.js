564f587fef3baa6cc050f6468b92aced53dd4d1b
raylano10 <143798089+raylano10@users.noreply.github.com>
2024-02-02 11:37:20 +0100

    new

--- opdracht-6/app.js
+++ opdracht-6/app.js
@@ -1,18 +1,16 @@
 let position = 0
-let blok = document.getElementById("blok")
+let blok = document.getElementById("block")
 
-function ReplaceBlok(){
+function moveBlock(){
 
     position += 20
     blok.style.left = position + "px"
-
+    
     if(position >= innerWidth){
-
         position = 0 
-
     }
 
 }
 
 
-setInterval(ReplaceBlok, 10);
+setInterval(moveBlock, 10);
