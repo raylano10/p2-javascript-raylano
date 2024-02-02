90813eab0e36b74f362d0ce2feef25f7f449de1d
raylano10 <143798089+raylano10@users.noreply.github.com>
2024-02-02 16:15:47 +0100

    new

--- opdracht-7/app.js
+++ opdracht-7/app.js
@@ -1,12 +1,12 @@
-let button = document.getElementById("blok")
-let color = ["green", "red", "blue", "purple", "yellow"];
+let button = document.getElementById("cube")
+let colors = ["purple", "pink", "red", "green", "cyan"];
 
 
 button.addEventListener("click", function(){
-    
-    let random_cijfer = Math.floor(Math.random()*5)
-
-    button.style.backgroundColor = colors[random_cijfer]
-
+  
+    let random_Getal = Math.floor(Math.random()*5)
+  
+    button.style.backgroundColor = colors[random_Getal];
 }
+
 )
